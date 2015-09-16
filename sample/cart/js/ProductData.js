dbfad7aaa5ec411f78aeaf43bae6ca47d5b77887
kohei-kp <cowhey1991@gmail.com>
2015-09-17 05:49:58 +0900

    Mock Data

---
+++ sample/cart/js/ProductData.js
@@ -0,0 +1,32 @@
+module.exports = {
+  // Load Mock Product Data Into localStorage
+  init() {
+    localStorage.clear();
+    localStorage.setItem('product', JSON.stringify([
+      {
+        id: '0011001',
+        name: 'Anco',
+        image: 'anco.png',
+        description: "This is Anco's Maximin Liebkne',
+        variants: [
+          {
+            sku: '123123',
+            type: 'test1',
+            price: 4.99,
+            inventory: 1
+          }, {
+            sku: '123124',
+            type: 'test2',
+            price: 12.99,
+            inventory: 5
+          }, {
+            sku: '123125',
+            type: 'test3',
+            price: 19.99,
+            inventory: 3
+          }
+        ]
+      }
+    ]));
+  }
+};
